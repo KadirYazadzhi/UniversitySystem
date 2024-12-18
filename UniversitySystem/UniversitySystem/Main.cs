bf6b586fda128c3f4465dccd8e41defe6ebbb795
Kadir Yazadzhi <smx590751@gmail.com>
2024-12-18 15:06:47 +0200

    Update Main.cs

--- UniversitySystem/UniversitySystem/Main.cs
+++ UniversitySystem/UniversitySystem/Main.cs
@@ -5,6 +5,14 @@ using System.Collections.Generic;
 using System.Linq;
 
 class Start {
+    public static List<Admin> Admins = new List<Admin>();
+    public static List<Student> Students = new List<Student>();
+    public static List<Professor> Professors = new List<Professor>();
+    public static List<Program> Programs = new List<Program>();
+    public static List<Subject> Subjects = new List<Subject>();
+    public static List<Course> Courses = new List<Course>();
+    public static List<Schedule> Schedules = new List<Schedule>();
+    
     public static void Main(string[] args) {
         
     }
