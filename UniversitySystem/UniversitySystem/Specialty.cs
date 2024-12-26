2e39eb8a68edcdd4f818a8bb4f8a3b92d59dd9b1
Kadir Yazadzhi <smx590751@gmail.com>
2024-12-26 05:05:42 +0200

    Update Specialty.cs

--- UniversitySystem/UniversitySystem/Specialty.cs
+++ UniversitySystem/UniversitySystem/Specialty.cs
@@ -18,4 +18,19 @@ public class Specialty {
     public void AddCourse(Course course) {
         AvailableCourses.Add(course);
     }
+
+    public static void ViewAllSpecialties() {
+        Console.WriteLine("All specialties:");
+        foreach (string name in Start.Specialties.Select(s => s.Name)) {
+            Console.WriteLine($"    {name}");
+        }
+    }
+
+    public bool isSpecialtyExist() {
+        if (Start.Specialties.Any(s => s.Name == this.Name)) {
+            return true;
+        }
+        Console.WriteLine("This specialty does not exist.");
+        return false;
+    }
 }
