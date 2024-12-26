62ba723196ea9987b41f67f005dd433fcb21159d
Kadir Yazadzhi <smx590751@gmail.com>
2024-12-26 02:21:18 +0200

    Update Student.cs

--- UniversitySystem/UniversitySystem/Student.cs
+++ UniversitySystem/UniversitySystem/Student.cs
@@ -2,16 +2,17 @@ namespace UniversitySystem;
 
 public class Student : User {
     public string FullName { get; set; }
-    public string Speciality { get; set; }
+    public Specialty Speciality { get; set; }
     public Program Program { get; set; }
     public List<Course> EnrolledCourses { get; set; }
     public Dictionary<Subject, double> Grades { get; set; }
     public double Discount { get; set; }
 
-    public Student(int id, string username, string email, string password, string role, string fullName, string speciality, Program program, List<Course> enrolledCourses)
+    public Student(int id, string username, string email, string password, string role, string fullName, Specialty specialty, Program program, List<Course> enrolledCourses)
         : base(id, username, email, password, role) {
         FullName = fullName;
-        Speciality = speciality;
+        Speciality = specialty;
+        specialty.AddStudent(this);
         Program = program;
         EnrolledCourses = enrolledCourses ?? new List<Course>();
         Grades = new Dictionary<Subject, double>();
